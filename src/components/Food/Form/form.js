c6806ccfa81d2c75a6fab470039425b5ffbbba31
Becky Jones <beckyljones151192@gmail.com>
2023-04-10 17:33:20 +0100

    addedd to database code done but messy, form not in own component

--- src/components/Food/Form/form.js
+++ src/components/Food/Form/form.js
@@ -1,18 +1,32 @@
 import React from "react";
 
-function Form() {
+function Form({ food, handleSubmit }) {
   return (
     <div>
       <h6>Form Page</h6>
       <div>
-        <form autoComplete="off">
-          <input type="text" placeholder="Food..." />
+        <form
+          autoComplete="off"
+          //   onSubmit={(event) => {
+          //     handleSubmit(event);
+          //   }}
+        >
+          <input name="formFoodItem" type="text" placeholder="Food..." />
           <br />
-          <input type="text" placeholder="Where/Recipe..." />
+          <br />
+          {/* <input type="text" placeholder="Where/Recipe..." /> */}
+          <textarea type="text" placeholder="Where/Recipe..." />
           <br />
           <input type="text" placeholder="Recommmended By..." />
           <br />
-          <button type="submit">Submit</button>
+          <button
+            type="submit"
+            onSubmit={(event) => {
+              handleSubmit(event);
+            }}
+          >
+            Submit
+          </button>
           <br />
         </form>
       </div>
--- src/components/Food/food.js
+++ src/components/Food/food.js
@@ -7,12 +7,74 @@ import {
   collection,
   onSnapshot,
   doc,
+  addDoc,
   deleteDoc,
 } from "firebase/firestore";
 
 function Food() {
   const [food, setNewFood] = useState([]);
+  const [form, setForm] = useState({});
+  // const [foodItemInput, setFoodItemInput] = useState("");
+  const [updatePage, setUpdatePage] = useState(false);
   console.log({ food });
+
+  function handleChange(event) {
+    let inputName = event.target.name;
+    // console.log({ inputName });
+    let vl = event.target.value;
+    // console.log({ vl });
+    console.log({ form });
+    setForm({ ...form, [event.target.id]: event.target.value });
+    console.log({ form });
+  }
+
+  async function handleSubmit(event) {
+    // refreshes form, empties form inputs
+    // window.location.reload();
+    console.log(form);
+    console.log("submit button pressed");
+    event.preventDefault();
+    console.log(form.formFooditem);
+    await addDoc(collection(db, "Food"), {
+      beckyopinion: "🤷",
+      fooditem: form.formFoodItem,
+      recipe: form.formFoodRecipe,
+      recommendedby: form.formFoodRecommend,
+      tried: false,
+      // fooditem: foodItemInput,
+    });
+  }
+
+  ////CRUD
+  // Create/add to database
+  // const createFood = async (event) => {
+  //   event.preventDefault(event);
+  //   await addDoc(collection(db, "Food"), {
+  //     beckyopinion: "🤷",
+  //     tried: false,
+  //     fooditem: formFoodItem,
+  //   });
+  // };
+
+  /*
+  function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  // create
+  const createTodo = async (event) => {
+    event.preventDefault(event);
+    if (input === "") {
+      alert("Please enter");
+      return;
+    }
+    await addDoc(collection(db, "todo"), {
+      text: input,
+      completed: false,
+    });
+    setInput("");
+  }; 
+  */
 
   /// Read/get all
   useEffect(() => {
@@ -34,20 +96,59 @@ function Food() {
     await deleteDoc(doc(db, "Food", id));
   };
 
-  // delete
-  //  const deleteTodo = async (id) => {
-  //   await deleteDoc(doc(db, "todo", id));
-  // };
-
   return (
     <div className="foodPage">
       <div>
         <h3>Recommend to Becky</h3>
         <h4>Food Page</h4>
       </div>
-      <div>
-        <Form />
-      </div>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <input
+          onChange={(event) => {
+            handleChange(event);
+          }}
+          name="formFoodItem"
+          id="formFoodItem"
+          // value={foodItemInput}
+          type="text"
+          placeholder="Food..."
+        />
+        <br />
+        <br />
+        {/* <input type="text" placeholder="Where/Recipe..." /> */}
+        <textarea
+          name="formFoodRecipe"
+          id="formFoodRecipe"
+          onChange={(event) => {
+            handleChange(event);
+          }}
+          type="text"
+          placeholder="Where/Recipe..."
+        />
+        <br />
+        <input
+          onChange={(event) => {
+            handleChange(event);
+          }}
+          name="formFoodRecommend"
+          id="formFoodRecommend"
+          type="text"
+          placeholder="Recommmended By..."
+        />
+        <br />
+        <button
+          type="submit"
+          // onSubmit={(event) => {
+          //   handleSubmit(event);
+          // }}
+        >
+          Submit
+        </button>
+        <br />
+      </form>
+      {/* <div>
+        <Form food={food} submitForm={handleSubmit} /> 
+      </div> */}
       <div>
         <FoodTable food={food} deleteFood={deleteFood} />
       </div>
