c6dec8527d5044fb0aba3490ca73d27f30b99aab
Becky Jones <beckyljones151192@gmail.com>
2023-04-13 14:23:52 +0100

    commented out set data from file and added to database via firebase, done some css

--- src/components/dataFile/dataFood.js
+++ src/components/dataFile/dataFood.js
@@ -1,54 +1,54 @@
 let food = [
-  {
-    food: "McDonalds McPlant burger",
-    recipe: "McDonalds",
-    recommendedBy: "Me",
-    isDone: false,
-    beckyOpinion: "🤷‍♀️",
-  },
-  {
-    food: "McDonalds McVeggie burger",
-    recipe: "McDonalds",
-    recommendedBy: "Me",
-    isDone: false,
-    beckyOpinion: "🤷‍♀️",
-  },
-  {
-    food: "Deep fried Mars bar",
-    recipe:
-      "At this rate Scotland (nowhere in Chelmsley Wood will deep fry my Mars bar)",
-    recommendedBy: "Max Bootcamp 4",
-    isDone: false,
-    beckyOpinion: "🤷‍♀️",
-  },
-  {
-    food: "US Biscuits",
-    recipe: "https://www.allrecipes.com/recipe/20075/basic-biscuits/",
-    recommendedBy: "Liz.K (US) SoC Coach",
-    isDone: false,
-    beckyOpinion: "🤷‍♀️",
-  },
-  {
-    food: "Sushi",
-    recipe: "can't remember",
-    recommendedBy: "Emma Bootcamp 4",
-    isDone: false,
-    beckyOpinion: "🤷‍♀️",
-  },
-  {
-    food: "Tofu",
-    recipe: "Different dishes",
-    recommendedBy: "Amy B13 Technology",
-    isDone: false,
-    beckyOpinion: "🤷‍♀️",
-  },
-  {
-    food: "Smores",
-    recipe: "Kit from store",
-    recommendedBy: "Me (+ US TV)",
-    isDone: false,
-    beckyOpinion: "🤷‍♀️",
-  },
+  // {
+  //   fooditem: "McDonalds McPlant burger",
+  //   recipe: "McDonalds",
+  //   recommendedby: "Me",
+  //   tried: false,
+  //   beckyOpinion: "🤷‍♀️",
+  // },
+  // {
+  //   fooditem: "McDonalds McVeggie burger",
+  //   recipe: "McDonalds",
+  //   recommendedby: "Me",
+  //   tried: true,
+  //   beckyopinion: "🤫",
+  // },
+  // {
+  //   fooditem: "Deep fried Mars bar",
+  //   recipe:
+  //     "At this rate Scotland (nowhere in Chelmsley Wood will deep fry my Mars bar)",
+  //   recommendedby: "Max Bootcamp 4",
+  //   tried: false,
+  //   beckyopinion: "🤷‍♀️",
+  // },
+  // {
+  //   fooditem: "US Biscuits",
+  //   recipe: "https://www.allrecipes.com/recipe/20075/basic-biscuits/",
+  //   recommendedby: "Liz.K (US) SoC Coach",
+  //   tried: false,
+  //   beckyopinion: "🤷‍♀️",
+  // },
+  // {
+  //   fooditem: "Sushi",
+  //   recipe: "can't remember",
+  //   recommendedby: "Emma Bootcamp 4",
+  //   tried: false,
+  //   beckyopinion: "🤷‍♀️",
+  // },
+  // {
+  //   fooditem: "Tofu",
+  //   recipe: "Different dishes",
+  //   recommendedby: "Amy B13 Technology",
+  //   tried: false,
+  //   beckyopinion: "🤷‍♀️",
+  // },
+  // {
+  //   fooditem: "Smores",
+  //   recipe: "Kit from store",
+  //   recommendedby: "Me (+ US TV)",
+  //   tried: false,
+  //   beckyopinion: "🤷‍♀️",
+  // },
 ];
 
 module.exports = food;
