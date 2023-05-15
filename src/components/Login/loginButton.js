b41be0cb6ee5275a342ecc010d3c8c1a2605c78b
Beckster6211 <beckyljones151192@gmail.com>
2023-05-15 20:46:53 +0100

    firebase auth create a user

--- src/components/Login/loginButton.js
+++ src/components/Login/loginButton.js
@@ -1,9 +1,55 @@
-import React from "react"
+import React, {useState} from "react"
+import {authentication} from "../../firebase"
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth"
 
 function Login(){
+
+    const [signIn, setSignIn] = useState(false)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    function beckyEmail(event){
+        console.log(event.target.value)
+        setEmail(event.target.value)
+    }
+
+    function beckyPassword(event){
+        console.log(event.target.value)
+        setPassword(event.target.value)
+    } 
+
+    const createUser = () => {
+        createUserWithEmailAndPassword(authentication, email, password).then((userCredential) => {
+    // Signed in 
+    console.log(userCredential)
+    const user = userCredential.user;
+    console.log(user)
+  }).catch((error) => {
+    const errorCode = error.code;
+    console.log(errorCode)
+    const errorMessage = error.message;
+    console.log(errorMessage)
+  });
+    // const signInUser = () =>{
+    //     signInWithEmailAndPassword(authentication, email, password).then((userCredential)=>{
+    //         console.log(userCredential)
+    //         const user = userCredential.user
+    //         console.log({user})
+    //     }).catch((error)=>{
+    //         console.log(error)
+    //     })
+    }
+
 return (
     <div>
+        
         <button>login</button>
+        <br/>
+        <input name = "email" placeholder = "Email" onChange = {(event)=>{beckyEmail(event)}}/>
+        <input name="password" placeholder ="Password" onChange = {(event) => {beckyPassword(event)}}/>
+        {/* <input value = {email} placeholder = "Email" onChange = {text => setEmail(text)}/> */}
+        {/* <input value = {password} placeholder ="Password" onChange = {text => setPassword(text)}/> */}
+        <button onClick={createUser}>Sign In</button>
     </div>
 )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,10 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// auth code
+import {getAuth} from "firebase/auth"
+
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD06k4HH_wcghZS2nl28DTdlgy0HkeXNlA",
@@ -16,4 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+//const db = getFirestore(app) // was export const {db} = Firestore(app)
+// auth code 
+export const authentication = getAuth(app)
+export const db = getFirestore(app)
