c0f514d9a38c96d4b7814ed00f2b49c787923245
Beckster6211 <beckyljones151192@gmail.com>
2023-05-17 13:16:37 +0100

    made so checks if me and displays a p tag, may have to all the log in in app and pass everything down

--- src/components/Login/loginButton.js
+++ src/components/Login/loginButton.js
@@ -1,76 +1,80 @@
 import React, {useState} from "react"
-import {authentication} from "../../firebase"
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth"
+// import {authentication} from "../../firebase"
+// import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth"
+
+import LoginForm from "./loginForm"
 
 function Login(){
 
-    const [signIn, setSignIn] = useState(false)
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+    // const [signIn, setSignIn] = useState(false)
+    // const [email, setEmail] = useState("")
+    // const [password, setPassword] = useState("")
 
-    // console.log(signIn)
-    function getEmail(event){
-        console.log(event.target.value)
-        setEmail(event.target.value)
-    }
+    // // console.log(signIn)
+    // function getEmail(event){
+    //     console.log(event.target.value)
+    //     setEmail(event.target.value)
+    // }
 
-    function getPassword(event){
-        console.log(event.target.value)
-        setPassword(event.target.value)
-    } 
+    // function getPassword(event){
+    //     console.log(event.target.value)
+    //     setPassword(event.target.value)
+    // } 
 
-    const createUser = () => {
-        createUserWithEmailAndPassword(authentication, email, password).then((userCredential) => {
-    // Signed in 
-    console.log(userCredential)
-    const user = userCredential.user;
-    console.log(user)
-  }).catch((error) => {
-    const errorCode = error.code;
-    console.log(errorCode)
-    const errorMessage = error.message;
-    console.log(errorMessage)
-  });
-    }
+//     const createUser = () => {
+//         createUserWithEmailAndPassword(authentication, email, password).then((userCredential) => {
+//     // Signed in 
+//     console.log(userCredential)
+//     const user = userCredential.user;
+//     console.log(user)
+//   }).catch((error) => {
+//     const errorCode = error.code;
+//     console.log(errorCode)
+//     const errorMessage = error.message;
+//     console.log(errorMessage)
+//   });
+//     }
     
-    const signInUser = () => {
-        signInWithEmailAndPassword(authentication, email, password).then((userCredential)=>{
-            console.log(userCredential)
-            const user = userCredential.user
-            console.log({user})
-            setSignIn(true)
-        }).catch((error)=>{
-            console.log(error)
-        })
-    }
+    // const signInUser = () => {
+    //     signInWithEmailAndPassword(authentication, email, password).then((userCredential)=>{
+    //         console.log(userCredential)
+    //         const user = userCredential.user
+    //         console.log({user})
+    //         setSignIn(true)
+    //     }).catch((error)=>{
+    //         console.log(error)
+    //     })
+    // }
 
-    const signOutUser = () => {
-        signOut(authentication).then((response)=>{
-            //sign out successful
-            setSignIn(false)
-            console.log("sign out successful")
-        }).catch((error)=>{
-            console.log(error)
-        })
-    }
+    // const signOutUser = () => {
+    //     signOut(authentication).then((response)=>{
+    //         //sign out successful
+    //         setSignIn(false)
+    //         console.log("sign out successful")
+    //     }).catch((error)=>{
+    //         console.log(error)
+    //     })
+    // }
 
 return (
     <div>
         
         <button>login</button>
         <br/>
-        <input name = "email" placeholder = "Email" onChange = {(event)=>{getEmail(event)}}/>
+        <LoginForm/>
+        {/* <input name = "email" placeholder = "Email" onChange = {(event)=>{getEmail(event)}}/>
         <input name="password" placeholder ="Password" onChange = {(event) => {getPassword(event)}}/>
-        <button onClick={createUser}>Create User</button>
+        <button onClick={createUser}>Create User</button> */}
         {/* <input value = {email} placeholder = "Email" onChange = {text => setEmail(text)}/> */}
         {/* <input value = {password} placeholder ="Password" onChange = {text => setPassword(text)}/> */}
-        {signIn === true ? 
+        {/* {signIn === true ? 
         <button onClick={signOutUser}>Sign Out</button>
         : 
         <button onClick={signInUser}>Sign In</button>
-        }
-        
+        } */}
+        <br/>
     </div>
+    
 )
 }
 
---
+++ src/components/Login/loginForm.js
@@ -0,0 +1,94 @@
+import React, {useState} from "react"
+import {authentication} from "../../firebase"
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from "firebase/auth"
+
+function LoginForm(){
+    const [becky, setBecky] = useState(false)
+    const [signIn, setSignIn] = useState(false)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    
+    // console.log(signIn)
+    function getEmail(event){
+        console.log(event.target.value)
+        setEmail(event.target.value)
+    }
+
+    function getPassword(event){
+        console.log(event.target.value)
+        setPassword(event.target.value)
+    } 
+
+    const createUser = () => {
+        createUserWithEmailAndPassword(authentication, email, password).then((userCredential) => {
+    // Signed in 
+    console.log(userCredential)
+    const user = userCredential.user;
+    console.log(user)
+    
+  }).catch((error) => {
+    const errorCode = error.code;
+    console.log(errorCode)
+    const errorMessage = error.message;
+    console.log(errorMessage)
+   
+  });
+    }
+
+    const signInUser = () => {
+        signInWithEmailAndPassword(authentication, email, password).then((userCredential)=>{
+            console.log(userCredential)
+            let user = userCredential.user
+            console.log({user})
+            if(user.uid === "tFvXi6smSTTgohw9kQMQrPIGUZ22"){
+             setBecky(true)
+            }
+            setSignIn(true)
+            // window.location.reload();
+        }).catch((error)=>{
+            console.log(error)
+        })
+    }
+
+    const signOutUser = () => {
+        signOut(authentication).then((response)=>{
+            //sign out successful
+            setSignIn(false)
+            console.log("sign out successful")
+            
+            // window.location.reload();
+        }).catch((error)=>{
+            console.log(error)
+        })
+
+    }    
+return (
+    <div>
+        
+        {/* <button>login</button> */}
+        <br/>
+        <button onClick={createUser}>Create User</button>
+        <br/>
+        <input type = "email" placeholder = {"Email..."} autoComplete="off" onChange = {(event)=>{getEmail(event)}}/>
+        &nbsp;
+        <input type="password" placeholder ="Password" autoComplete="off" onChange = {(event) => {getPassword(event)}}/>
+       
+        {/* <button onClick={createUser}>Create User</button> */}
+        <br/>
+        {/* {signIn === true ? 
+        <button onClick={signOutUser}>Sign Out</button>
+        : 
+        <button onClick={signInUser}>Sign In</button>
+        } */}
+        <button onClick={signInUser}>Sign In</button>
+        <button onClick={signOutUser}>Sign Out</button>
+        {becky === true ? 
+        <p>"Hi Becky"</p>
+        : <p>"Hello someone else"</p>
+    }
+    </div>
+)
+}
+
+export default LoginForm
