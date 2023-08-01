20b2fb242cc15dbce4610392856ed89454eb6e56
Beckster6211 <beckyljones151192@gmail.com>
2023-08-01 19:12:11 +0100

    tidied table.js

--- src/components/Food/Table/table.js
+++ src/components/Food/Table/table.js
@@ -1,11 +1,14 @@
 import React from "react";
-import Item from "../Item/item";
 import "./table.css";
 
+//
+import Item from "../Item/item";
+
+//
 import Table from 'react-bootstrap/Table';
 import Container from "react-bootstrap/Container";
-import Card from "react-bootstrap/Card"
 
+//
 function FoodTable({userBecky, food, foodITried, handleOpinion, deleteFood }) {
   // console.log(food);
   return (
@@ -14,13 +17,14 @@ function FoodTable({userBecky, food, foodITried, handleOpinion, deleteFood }) {
     className="foodTableComponent p-0"
     // style={{height:"55vw"}}
     >
-      {/* <Container fluid className="border rounded p-1"> */}
       <Table 
       striped 
       bordered 
       hover 
       className="foodTable border border-dark border-4 table-fixed"
-      // style={{width:""}}
+      style={{
+
+      }}
       >
       <thead 
       className="tableHead bg-dark-subtle "
@@ -98,61 +102,6 @@ function FoodTable({userBecky, food, foodITried, handleOpinion, deleteFood }) {
           })}
         </tbody>
       </Table>
-      {/* </Container> */}
-      {/* /////////////////////// */}
-      {/* <details>
-      <table className="foodTable">
-        <thead className="tableHead"
-        
-        >
-          <tr>
-            <th className="tableHeader"
-            
-            >Food</th>
-            <th className="tableHeader">Where/Recipe?</th>
-            <th className="tableHeader">Becky Opinion</th> */}
-            {/* <th>Tried</th> */}
-            {/* <th className="tableHeader">Who recommends</th>
-            <th className="tableHeader">Delete</th>
-          </tr>
-        </thead>
-        <tfoot className="tableFoot">
-          <tr>
-            <th>Food</th>
-            <th>Where/Recipe?</th>
-            <th>Becky Opinion</th> */}
-            {/* <th>Tried</th> */}
-            {/* <th>Who recommends</th>
-            <th>Delete</th>
-          </tr>
-        </tfoot>
-        <tbody className="foodTableBody">
-          {food.map((item, index) => {
-            // console.log(item);
-            // console.log(item.tried);
-            // console.log(index);
-            return (
-              <Item
-                food={item}
-                foodItem={item.fooditem}
-                foodRecipe={item.recipe}
-                foodOpinion={item.beckyopinion}
-                foodId={item.id}
-                foodTried={item.tried}
-                foodRecommend={item.recommendedby}
-                index={index}
-                key={item.id}
-                removeFood={deleteFood}
-                triedIt={foodITried}
-                changeOpinion={handleOpinion}
-                becky={userBecky}
-              />
-            );
-          })}
-        </tbody>
-      </table>
-      </details> */}
-      {/* /////////////////////// */}
     </Container>
   );
 }
